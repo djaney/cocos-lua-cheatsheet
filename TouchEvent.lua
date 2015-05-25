dc6d59112dbd52f2bb59edc86659c624da40b98c
Djane Rey Mabelin <thedjaney@gmail.com>
2015-05-25 23:51:29 +0800

    Create TouchEvent.lua

---
+++ TouchEvent.lua
@@ -0,0 +1,5 @@
+    local listener = cc.EventListenerTouchOneByOne:create()
+    listener:registerScriptHandler(onTouchBegan,cc.Handler.EVENT_TOUCH_BEGAN)
+    listener:registerScriptHandler(onTouchMoved,cc.Handler.EVENT_TOUCH_MOVED)
+    listener:registerScriptHandler(onTouchEnded,cc.Handler.EVENT_TOUCH_ENDED)
+    self.node:getEventDispatcher():addEventListenerWithSceneGraphPriority(listener,self.node)
